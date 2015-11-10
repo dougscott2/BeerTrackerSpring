fae8ef5208a12513b36ea43c9e7e2012fa6c100e
DrScott <dougscott2@gmail.com>
2015-11-10 12:18:36 -0500

    initial import

---
+++ src/main/java/com/theironyard/BeerRepository.java
@@ -0,0 +1,11 @@
+package com.theironyard;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by DrScott on 11/10/15.
+ */
+public interface BeerRepository extends CrudRepository<Beer, Integer>{
+    
+
+}
